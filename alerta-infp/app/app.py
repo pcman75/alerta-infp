6563f11f71c9f99e9673a6eea59b15de66129c23
pcman75 <manoliu@gmail.com>
2022-07-22 09:48:00 +0300

    log

--- alerta-infp/app/app.py
+++ alerta-infp/app/app.py
@@ -4,77 +4,80 @@ import paho.mqtt.client as mqtt
 from sseclient import SSEClient
 
 def main():
-    configfile = pathlib.Path('/data/options.json')
-    if not configfile.exists():
-        configfile = pathlib.Path(__file__).parent.parent / 'config.json'
+    try:
+        configfile = pathlib.Path('/data/options.json')
+        if not configfile.exists():
+            configfile = pathlib.Path(__file__).parent.parent / 'config.json'
 
-    with open(configfile) as f:
-        config = json.load(f)
+        with open(configfile) as f:
+            config = json.load(f)
 
-    if 'options' in config:
-        config = config['options']
+        if 'options' in config:
+            config = config['options']
 
-    logger = logging.getLogger()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging._nameToLevel[config['LOG_LEVEL']])
+        logger = logging.getLogger()
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+                '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging._nameToLevel[config['LOG_LEVEL']])
 
-    mqttClient = mqtt.Client("alerta-infp")
-    mqttClient.username_pw_set(config["mqtt_user"], config["mqtt_password"])
-    mqttClient.will_set("alerta-infp/online", "offline", retain = True, qos = 0)
-    
-    mqttClient.connect(config["mqtt_server"], config["mqtt_port"])
-    mqttClient.loop_start()
-    
-    mqttClient.publish("alerta-infp/online", "online", retain = True, qos = 0)
-    mqttClient.publish("homeassistant/binary_sensor/alerta-infp/config", '{"name":"Cutremur","dev_cla":"safety","stat_t":"homeassistant/binary_sensor/alerta-infp/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
-    mqttClient.publish("homeassistant/sensor/alerta-infp/magnitudine/config", '{"name":"Magnitudine Cutremur","stat_t":"homeassistant/sensor/alerta-infp/magnitudine/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
-    mqttClient.publish("homeassistant/sensor/alerta-infp/seconds/config", '{"name":"Secunde pana la Bucuresti","stat_t":"homeassistant/sensor/alerta-infp/seconds/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
-    
-    prev_earthquake = 'ON'
-    prev_magnitude = 0.
-    prev_seconds = 0.
+        mqttClient = mqtt.Client("alerta-infp")
+        mqttClient.username_pw_set(config["mqtt_user"], config["mqtt_password"])
+        mqttClient.will_set("alerta-infp/online", "offline", retain = True, qos = 0)
+        
+        mqttClient.connect(config["mqtt_server"], config["mqtt_port"])
+        mqttClient.loop_start()
+        
+        mqttClient.publish("alerta-infp/online", "online", retain = True, qos = 0)
+        mqttClient.publish("homeassistant/binary_sensor/alerta-infp/config", '{"name":"Cutremur","dev_cla":"safety","stat_t":"homeassistant/binary_sensor/alerta-infp/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
+        mqttClient.publish("homeassistant/sensor/alerta-infp/magnitudine/config", '{"name":"Magnitudine Cutremur","stat_t":"homeassistant/sensor/alerta-infp/magnitudine/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
+        mqttClient.publish("homeassistant/sensor/alerta-infp/seconds/config", '{"name":"Secunde pana la Bucuresti","stat_t":"homeassistant/sensor/alerta-infp/seconds/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
+        
+        prev_earthquake = 'ON'
+        prev_magnitude = 0.
+        prev_seconds = 0.
 
-    while(1):
-        host = 'http://alerta.infp.ro/'
-        response = requests.get(host)    
-        key = re.search(r"(?sm)EventSource\('server\.php\?keyto=([a-z0-9]+)'\);", response.text)
-        if key:
-            messages = SSEClient(f'{host}server.php?keyto={key.groups()[0]}')
-            for msg in messages:
-                try:
-                    if(msg.data):
-                        message = json.loads(msg.data)
-                        if('err' in message):
-                            break;
-                        else:
-                            magnitude = float(message["mag"])
-                            earthquake = 'ON' if magnitude >= 1. else 'OFF'
-                            seconds = float(message["sec"])
+        while(1):
+            host = 'http://alerta.infp.ro/'
+            response = requests.get(host)    
+            key = re.search(r"(?sm)EventSource\('server\.php\?keyto=([a-z0-9]+)'\);", response.text)
+            if key:
+                messages = SSEClient(f'{host}server.php?keyto={key.groups()[0]}')
+                for msg in messages:
+                    try:
+                        if(msg.data):
+                            message = json.loads(msg.data)
+                            if('err' in message):
+                                break;
+                            else:
+                                magnitude = float(message["mag"])
+                                earthquake = 'ON' if magnitude >= 1. else 'OFF'
+                                seconds = float(message["sec"])
 
-                            logger.debug(f'Magnitude = {magnitude} seconds = {seconds} earthquake = {earthquake}')
+                                logger.debug(f'Magnitude = {magnitude} seconds = {seconds} earthquake = {earthquake}')
 
-                            if not math.isclose(prev_magnitude, magnitude):
-                                mqttClient.publish('homeassistant/sensor/alerta-infp/magnitudine/state', magnitude, qos = 0)
-                                logger.info(f'Magnitude = {magnitude}')
-                            
-                            if prev_earthquake != earthquake:
-                                mqttClient.publish('homeassistant/binary_sensor/alerta-infp/state', earthquake, qos = 0)
-                                logger.info(f'earthquake = {earthquake}')
-                            
-                            if not math.isclose(prev_seconds, seconds):
-                                mqttClient.publish('homeassistant/sensor/alerta-infp/seconds/state', seconds, qos = 0)
-                                logger.info(f'seconds = {seconds}')
+                                if not math.isclose(prev_magnitude, magnitude):
+                                    mqttClient.publish('homeassistant/sensor/alerta-infp/magnitudine/state', magnitude, qos = 0)
+                                    logger.info(f'Magnitude = {magnitude}')
+                                
+                                if prev_earthquake != earthquake:
+                                    mqttClient.publish('homeassistant/binary_sensor/alerta-infp/state', earthquake, qos = 0)
+                                    logger.info(f'earthquake = {earthquake}')
+                                
+                                if not math.isclose(prev_seconds, seconds):
+                                    mqttClient.publish('homeassistant/sensor/alerta-infp/seconds/state', seconds, qos = 0)
+                                    logger.info(f'seconds = {seconds}')
 
-                            prev_magnitude = magnitude
-                            prev_earthquake = earthquake
-                            prev_seconds = seconds
+                                prev_magnitude = magnitude
+                                prev_earthquake = earthquake
+                                prev_seconds = seconds
 
-                except Exception as e:
-                    print(e)
+                    except Exception as e:
+                        logger.error(e)
+    except Exception as e:
+        logger.error(e)
 
 if __name__ == '__main__':
     main()
